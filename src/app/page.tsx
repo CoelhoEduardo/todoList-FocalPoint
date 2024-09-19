a0b3c3551a071ba2a073691fbb1aa07964e53b18
Eduardo Coelho <educs98@outlook.com>
2024-09-19 17:54:25 -0300

    init header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-
+import { Header } from "@/components/Header/Header";
 
 export default function Home() {
-  return <h1>Hello</h1>;
+  return <Header />;
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,1 +1,12 @@
+import React from "react";
+import styles from "./Header.module.scss";
 
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>FocalPoint</div>
+      <div className={styles.welcome}>Bem-vindo de Volta, Marcus</div>
+      <div className={styles.date}>Segunda, 01 de Dezembro de 2025</div>
+    </header>
+  );
+};
